1b119ebf0f6633eede5b1a850f10d2e62349ebe4
migueleichler <migueleichler23@gmail.com>
2021-04-18 12:18:09 -0300

    api_valor_do_metro_quadrado: corrigido leitura das variáveis de ambiente de teste

--- api_valor_do_metro_quadrado/tests/helpers.py
+++ api_valor_do_metro_quadrado/tests/helpers.py
@@ -7,6 +7,6 @@ from dotenv import dotenv_values
 
 @pytest.fixture
 def mock_env_vars():
-    config = dotenv_values(".dev-env")
+    config = dotenv_values("./api_valor_do_metro_quadrado/.dev-env")
     with mock.patch.dict(os.environ, config):
         yield
